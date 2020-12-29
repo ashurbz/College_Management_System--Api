22e6ee2b0a50314f932218bc0b5ca265d0fb1704
ashurbz <rbzashu1@gmail.com>
2020-12-29 16:14:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const db = require('./config/mongoose');
+const port=process.env.PORT||8000;
+
+app.use(express.urlencoded());
+
+app.get('/',(req,res)=>{
+    res.send ("hiii")
+})
+
+
+
+
+
+
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(`Server is not running Error: ${err}`)
+    }
+    console.log(`Server is running on port: ${port}`);
+})
