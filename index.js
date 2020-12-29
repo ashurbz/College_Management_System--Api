de01484f5903e52ad54ac63d525ea97ad56493f2
ashurbz <rbzashu1@gmail.com>
2020-12-29 22:16:05 +0530

    completed sign in

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-const { urlencoded } = require('body-parser');
+// const  bodyParser  = require('body-parser');
 const express = require('express');
+// const formData = require("express-form-data");
 const app = express();
 const db = require('./config/mongoose');
 const port=process.env.PORT||8000;
+const passport = require('passport');
+// var multer = require('multer');
+// var upload = multer();
+var bodyParser = require('body-parser');
+const {upload} =require("./api/upload")
+const path =require("path")
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());  
+// in latest body-parser use like below.
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(formData.union());
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.json());
 
 
 
 
+// app.use('/',require('./routes'));
+app.post("/upload",upload)
+ 
 
-app.use('/',require('./routes'));
+
+
 
 
 
@@ -22,5 +40,5 @@ app.listen(port,(err)=>{
     if(err){
         console.log(`Server is not running Error: ${err}`)
     }
-    console.log(`Server is running on port: ${port}`);
+    console.log(`Server is running on port: ${port}`); 
 })
